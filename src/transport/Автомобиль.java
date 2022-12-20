7ecccc708b8f5f472b3865349a7d7c3daf319e83
Rumonz <lone_assassin@mail.ru>
2022-12-20 15:16:20 +0300

    Точно выполнил задание 1

--- src/transport/Автомобиль.java
+++ src/transport/Автомобиль.java
@@ -3,110 +3,69 @@ package transport;
 import java.time.LocalDate;
 
 public class Автомобиль {
-    private  String brand;
-    private  String model;
-    public double engineVolume;
-    public String color;
-    private  int year;
-    private  String country;
+    private final String brand;
+    private final String model;
+    private double engineVolume;
+    private String color;
+    private final int year;
+    private final String country;
     //
-    public String transmission;
-    private  String bodyType;
-    public String registrationNumber;
-    private  int seatsNumber;
-    public String wheelsSummerOrWinter;
+    private String transmission;
+    private final String bodyType;
+    private String registrationNumber;
+    private final int seatsNumber;
+    private boolean summerWheels;
 
 
     public Автомобиль(String brand, String model, int year, String country, String color, double engineVolume) {
-        this.brand = brand;
-        this.model = model;
-        this.year = year;
-        this.country = country;
-        this.color = color;
-        this.engineVolume = engineVolume;
         if (brand  == null || brand.isEmpty() || brand.isBlank()) {
-            this.brand = "default";
+            this.brand = "default";} else {
+            this.brand =brand;
         }
         if (model == null || model.isBlank() || model.isEmpty()) {
-            this.model = "default";
+            this.model = "default";}else {
+            this.model = model;
         }
         if (country == null || country.isEmpty() || country.isBlank()) {
-            this.country = "default";
+            this.country = "default";}else {
+            this.country = country;
         }
         if (engineVolume <= 0) {
-            this.engineVolume = 1.5;
+            this.engineVolume = 1.5;}else { this.engineVolume = engineVolume;
         }
         if (color == null || color.isBlank() || color.isEmpty()) {
-            this. color = "белый";
+            this. color = "белый";}else {this.color = color;
         }
         if (year <= 0) {
             this.year = 2000;
+        }else {
+            this.year = year;
         }
+        this.transmission = "АКП";
+        this.registrationNumber = "х000хх000";
+        this.bodyType = "Седан";
+        this.summerWheels = true;
+        this.seatsNumber = 4;
 
-    }
-    public Автомобиль(String brand, String model, int year, String country, String color, double engineVolume, String transmission, String bodyType, int seatsNumber, String registrationNumber, String wheelsSummerOrWinter) {
-        this.brand = brand;
-        this.model = model;
-        this.year = year;
-        this.country = country;
-        this.color = color;
-        this.engineVolume = engineVolume;
-        this.transmission = transmission;
-        this.bodyType = bodyType;
-        this.seatsNumber = seatsNumber;
-        this.registrationNumber = registrationNumber;
-        this.wheelsSummerOrWinter = wheelsSummerOrWinter;
-        if (brand == null|| brand.isEmpty() || brand.isBlank()) {
-            this.brand = "default";
-        }
-        if (model == null|| model.isBlank() || model.isEmpty()) {
-            this.model = "default";
-        }
-        if (country == null|| country.isEmpty() || country.isBlank()) {
-            this.country = "default";
-        }
-        if (engineVolume <= 0) {
-            this.engineVolume = 1.5;
-        }
-        if (color == null|| color.isBlank() || color.isEmpty()) {
-            this.color = "белый";
-        }
-        if (year <= 0) {
-            this.year = 2000;
-        }
-        if (transmission == null|| transmission.isBlank() || transmission.isEmpty()){
-            this.transmission = "default";
-        }
-        if (registrationNumber == null || registrationNumber.isBlank() || registrationNumber.isEmpty()) {
-            this.registrationNumber = "default";
-        }
-        if (bodyType == null || bodyType.isBlank() || bodyType.isEmpty()) {
-            this.bodyType = "default";
-        }
-        boolean summerOrWinter = LocalDate.now().getMonthValue() > 2 && LocalDate.now().getMonthValue() < 12;
-        if (summerOrWinter = true){
-            this.wheelsSummerOrWinter = "Летняя резина";
-        } else {
-            this.wheelsSummerOrWinter = "Зимняя резина";
-        }
-  //      if (LocalDate.now().getMonthValue() > 2 && LocalDate.now().getMonthValue() < 12) {
-  //          this.wheelsSummerOrWinter = "Летняя резина";
-   //     } else if (LocalDate.now().getMonthValue() == 12 || LocalDate.now().getMonthValue() == 1 || LocalDate.now().getMonthValue() == 2) {
-   //         this.wheelsSummerOrWinter = "Зимняя резина";
-  //      }
-        if (seatsNumber == 0) {
-            this.seatsNumber = 4;
-        }
     }
 
     public void changeWheels() {
-        if (LocalDate.now().getMonthValue() > 2 && LocalDate.now().getMonthValue() < 12 && wheelsSummerOrWinter == "Зимняя резина") {
-            System.out.println("Срочно поменяй резины на Летнюю!");
-        } else if (LocalDate.now().getMonthValue() <= 2 && LocalDate.now().getMonthValue() == 12 && wheelsSummerOrWinter == "Летняя резина") {
-            System.out.println("Срочно поменяй резину на Зимнюю!");
-        } else {
-            System.out.println("Менять резину пока не нужно!");
+        summerWheels = !summerWheels;
+
+    }
+
+    public boolean isCorrectRegNum() {
+        if (registrationNumber.length() != 9) {
+            return false;
         }
+        char chars[] = registrationNumber.toCharArray();
+        if (Character.isAlphabetic(chars[0]) || Character.isAlphabetic(chars[4]) || Character.isAlphabetic(chars[5])) {
+            return false;
+        }
+        if (!Character.isDigit(chars[1]) || !Character.isDigit(chars[2]) || !Character.isDigit(chars[3]) || !Character.isDigit(chars[6]) || !Character.isDigit(chars[7]) || !Character.isDigit(chars[8])) {
+            return false;
+        }
+        return true;
 
     }
 
@@ -155,7 +114,11 @@ public class Автомобиль {
     }
 
     public void setTransmission(String transmission) {
-        this.transmission = transmission;
+        if (transmission == null || transmission.isBlank() || transmission.isEmpty()) {
+            this.transmission = "АКП";
+        } else {
+            this.transmission = transmission;
+        }
     }
 
     public String getRegistrationNumber() {
@@ -163,18 +126,18 @@ public class Автомобиль {
     }
 
     public void setRegistrationNumber(String registrationNumber) {
-        this.registrationNumber = registrationNumber;
+        if (registrationNumber == null || registrationNumber.isBlank() || registrationNumber.isEmpty()) {
+            this.registrationNumber = "х000хх000";
+        } else {
+            this.registrationNumber = registrationNumber;
+        }
     }
 
-    public String getWheelsSummerOrWinter() {
-        return wheelsSummerOrWinter;
+    public boolean isSummerWheels() {
+        return summerWheels;
     }
 
-    public void setWheelsSummerOrWinter(String wheelsSummerOrWinter) {
-        this.wheelsSummerOrWinter = wheelsSummerOrWinter;
-    }
-
-    public String toString(){
-         return "Автомобиль - "+brand+" "+model+"."+" Год выпуска - "+year+"."+" Страна где собрано - "+country+"."+" Цвет  "+color+"."+" Объём двигателя - "+engineVolume+" литров. Кол-во мест: "+seatsNumber+". Тип кузова: "+bodyType+". Резина: "+wheelsSummerOrWinter+". Регистрационный номер: "+registrationNumber+". Коробка передач: "+transmission+".";
+    public void setSummerWheels(boolean summerWheels) {
+        this.summerWheels = summerWheels;
     }
 }
